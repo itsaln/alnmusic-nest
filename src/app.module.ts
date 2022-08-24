d389888faedcc714aa80ff7763fa174084275708
itsaln <itsaln23@gmail.com>
2022-08-25 00:52:30 +0500

    fileModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppController } from '@app/app.controller'
 import { AppService } from '@app/app.service'
 import { TrackModule } from '@app/track/track.module'
 import { FileModule } from '@app/file/file.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as path from 'path'
 
 @Module({
 	imports: [
+		ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
 		MongooseModule.forRoot('mongodb+srv://itsaln:WxvDPn1011@cluster0.hgr0sbm.mongodb.net/music-platform?retryWrites=true&w=majority'),
 		TrackModule,
 		FileModule
